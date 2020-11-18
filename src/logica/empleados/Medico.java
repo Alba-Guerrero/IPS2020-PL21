deb06e6bb5ef12d9ae4c39bd0d926e4e5128dafa
Raul <UO265210@uniovi.es>
2020-11-18 19:59:17 +0100

    Añadida la posibilidad de añadir empleados a citas, ademas de los filtros y carga la lista de enfermeros.

--- src/logica/empleados/Medico.java
+++ src/logica/empleados/Medico.java
@@ -25,7 +25,7 @@ public class Medico extends Empleado{
 	 * @param dJornada
 	 */
 	public Medico(String codeEmpleado,String nombre,String apellido, String pass, Time hInicio, Time hFin, java.util.Date date, java.util.Date date2, String dJornada) {
-		super(codeEmpleado, nombre,apellido,pass, hInicio, hFin, date, date2, dJornada);	
+		super(codeEmpleado,nombre,apellido,pass, hInicio, hFin, date, date2, dJornada);	
 	}
 
 	
